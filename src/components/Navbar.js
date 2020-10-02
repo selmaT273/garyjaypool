4e29814657ad52a3d89b6cdcaa0c08b9d716aef9
Stacey Teltser <stacey.teltser@gmail.com>
2020-10-01 20:41:39 -0500

    adjusted the navbar to add author, veteran, storyteller

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -53,7 +53,8 @@ function Navbar() {
         <>
             <nav className='navbar'>
                 <Link to='/' className='navbar-logo'>
-                    Gary Jay Pool
+                    <div>Gary Jay Pool</div> 
+                    <span>Author | Veteran | Storyteller</span>
                 </Link>
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
