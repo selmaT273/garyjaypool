3cfa28c685758cac22eb70c851a102ddc6b963cb
Stacey Teltser <stacey.teltser@gmail.com>
2020-10-01 19:11:39 -0500

    added responsiveness to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,7 +8,8 @@ export default function Home() {
                     <div className="home-pic-container">
                         <img className="home-pic" alt="explosives" src={require('../../home-image.jpg')} />
                         <div className="home-pic-text">
-                            <p>"I was out with a patrol in Viet Nam's rice paddy covered delta when it occurred to me...my father had spent his life trying to raise me to survive being a soldier in a war that he somehow knew was coming."</p>
+                            <div className="home-quote">"I was out with a patrol in Viet Nam's rice paddy covered delta when it occurred to me...my father had spent his life trying to raise me to survive being a soldier in a war that he somehow knew was coming."</div>
+                            <div className="home-author">-Gary Jay Pool, Xức Mây</div>
                         </div>
                     </div>
                 </div>
